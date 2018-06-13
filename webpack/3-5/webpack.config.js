7f4b493ca677aacc0366f24b0353a22b2c1d7b12
赵安东 <zhaoandong4213@sina.com>
2018-06-13 23:21:28 +0800

    6.13..

---
+++ webpack/3-5/webpack.config.js
@@ -0,0 +1,21 @@
+let webpack = require('webpack')
+let path = require('path')
+
+module.exports = {
+	entry: {
+		pageA: './src/pageA.js'
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].bundle.js',
+		chunkFilename: '[name].chunk.js'
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'common1',
+			minChunks: 2
+		})
+	]
+}
+
+
